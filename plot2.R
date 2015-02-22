88283a428e6dda74db3c6a4c77d4d20314c1e598
pratimkm <pratim.mukherjee@gmail.com>
2015-02-22 10:54:07 +0530

    some update in q2

--- plot2.R
+++ plot2.R
@@ -1,3 +1,6 @@
+#execute below code by doing
+#$ source("plot2.R")
+#$ showEmissionLevelByCity("24510")
 showEmissionLevelByCity <- function(fipsval){
   
   library(dplyr)
@@ -22,16 +25,18 @@ showEmissionLevelByCity <- function(fipsval){
   
   #filter data for baltimore city first
   NEI_wrapper <- tbl_df(NEI)
-  total_emission_by_year <- NEI_wrapper %>% filter(fips == fipsval)
+  total_emission <- NEI_wrapper %>% filter(fips == fipsval)
+ 
+  total_emission <- total_emission[,c(4,6)]
   
   # group by year to calculate total emission per year
-  total_emission_by_year <- aggregate(NEI$Emissions,by=list(NEI$year),sum)
-  
+  total_emission_by_year <- aggregate(total_emission,by=list(total_emission$year),sum)
+ 
   names(total_emission_by_year) <- c("Year","Emissions")
   
   # As per instruction plot with base plot func
-  barplot((total_emission_by_year$Emissions)/10^6,names.arg = total_emission_by_year$Year,
-          main="PM2.5 Emissions in Baltimore City, Maryland from 1999-2008",xlab = "Year",ylab = "Emission[in Million Tons]",col=c("green","yellow","red","violet"))
+  barplot((total_emission_by_year$Emissions),names.arg = total_emission_by_year$Year,
+          main="PM2.5 Emissions in Baltimore City, Maryland from 1999-2008",xlab = "Year",ylab = "Emission[in Tons]",col=c("green","yellow","red","violet"))
   
   dev.copy(png,file="plot2.png")
   dev.off()
